5a3783b8b3cfa96a3cccd18412b9392cb5970faa
Thibaut <thibaut@MacBook-Pro-de-Thibaut.local>
2018-10-03 17:51:43 +0200

    fix entries

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,6 @@
 const path = require('path');
 const slsw = require('serverless-webpack');
 
-const entries = {};
-
-Object.keys(slsw.lib.entries).forEach(
-	key => (entries[key] = ['./source-map-install.js', slsw.lib.entries[key]])
-);
-
 module.exports = {
 	mode: slsw.lib.webpack.isLocal ? 'development' : 'production',
 	entry: slsw.lib.entries,
